9e50411ccd9d0a712752595ade7a8f785c2f8f92
Vincent Rialland <vincent.rialland@gmail.com>
2018-06-01 22:30:36 +0200

    Test output to screen

--- clock/clock.ino
+++ clock/clock.ino
@@ -2,19 +2,31 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
+#include "SSD1306Wire.h"
+
 #include "config.h"
 
-
+// Display
+SSD1306Wire display(OLED_ADDRESS, OLED_SDA, OLED_SCL);
+// NTP
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, NTP_SERVER, 0, 3600);
 
 
 void setup() {
+  // Init display
+  display.init();
+  display.setFont(ArialMT_Plain_10);
+  
   // Connect to configured wifi
   Serial.begin(115200);
   Serial.println();
   Serial.print("Connecting to wifi: ");
   Serial.println(WIFI_SSID);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.drawStringMaxWidth(OLED_PADDING, 0, OLED_WIDTH, "Connecting to wifi: ");
+  display.drawStringMaxWidth(OLED_PADDING, 14, OLED_WIDTH, WIFI_SSID);
+  display.display();
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -24,6 +36,10 @@ void setup() {
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println();
+  display.clear();
+  display.drawStringMaxWidth(OLED_PADDING, 0, OLED_WIDTH, "Connected, IP address: ");
+  display.drawStringMaxWidth(OLED_PADDING, 14, OLED_WIDTH, String(WiFi.localIP()));
+  display.display();
   // Init NTP client
   timeClient.begin();
 }
@@ -31,7 +47,11 @@ void setup() {
 
 void loop() {
   timeClient.update();
-  Serial.println(timeClient.getFormattedTime());
+  display.clear();
+  String time = timeClient.getFormattedTime();
+  display.drawStringMaxWidth(OLED_PADDING, 0, OLED_WIDTH, time);
+  display.display();
+  Serial.println(time);
   delay(1000);
 }
 
--- clock/config.h
+++ clock/config.h
@@ -1,9 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// OLED pins
+#define OLED_ADDRESS 0x3c
+#define OLED_SDA SDA
+#define OLED_SCL SCL
+#define OLED_WIDTH 96
+#define OLED_HEIGHT 64
+#define OLED_PADDING 32
+
 // Wifi related
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "password"
 
 // NTP setting
 #define NTP_SERVER "pool.ntp.org"
